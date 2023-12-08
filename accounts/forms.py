3ff5819375912eed499c3d16c971f2c9ecfe9f45
ubiquitouss <ubiquitouss@yahoo.com>
2023-12-08 14:35:25 +0000

    chapter 9 is finished

---
+++ accounts/forms.py
@@ -0,0 +1,22 @@
+# accounts/forms.py
+from django.contrib.auth.forms import UserCreationForm,UserChangeForm
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+ class Meta(UserCreationForm):
+  model = CustomUser
+  fields = (
+ "username",
+ "email",
+ "age",
+ ) 
+  #fields = UserCreationForm.Meta.fields + ("age",)
+class CustomUserChangeForm(UserChangeForm):
+ class Meta:
+  model = CustomUser
+  #fields = UserChangeForm.Meta.fields
+  fields = (
+ "username",
+ "email",
+ "age",
+ ) # 
